b9fbcabd7dbd0e1d2dcf0c8970f0a72e29cb8831
louck <luc.parent@commown.coop>
2025-05-28 16:46:54 +0200

    Set the date_start of the contract when calling _modify_from_description
    I do not understand why it is necessary now, I have to dig it but it is
    a fields that we declare so I don't know why it should be updated
    automaticaly.

--- product_rental/models/contract.py
+++ product_rental/models/contract.py
@@ -237,6 +237,7 @@ class Contract(models.Model):
                 "sale_order_line_id": so_line.id,
             }
         )
+        self.date_start = NO_DATE
 
         rental_products = _rental_products(contract_descr)
 
